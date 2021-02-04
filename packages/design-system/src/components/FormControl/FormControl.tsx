ccf589f8a1a3f4b0999b5ae9375800f7f2e52a8a
bernardwang <bw.vbnm@gmail.com>
2021-02-04 14:03:47 -0600

    Rename FieldContainer to FormControl

--- packages/design-system/src/components/FormControl/FormControl.tsx
+++ packages/design-system/src/components/FormControl/FormControl.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 import classNames from 'classnames';
 import { uniqueId } from 'lodash';
 
-interface FieldContainerRenderProps {
+interface FormControlRenderProps {
   id: string;
   labelId: string;
   errorId: string;
   setRef: (elem: HTMLDivElement) => void;
 }
 
-interface FieldContainerProps {
+interface FormControlProps {
   /**
    * Additional classes to be added to the field container.
    */
@@ -72,11 +72,11 @@ interface FieldContainerProps {
   /**
    * A function that returns a field input element to accept render props
    */
-  render: (renderProps: FieldContainerRenderProps) => React.ReactNode;
+  render: (renderProps: FormControlRenderProps) => React.ReactNode;
 }
 
-export class FieldContainer extends React.Component<FieldContainerProps> {
-  constructor(props: FieldContainerProps) {
+export class FormControl extends React.Component<FormControlProps> {
+  constructor(props: FormControlProps) {
     super(props);
 
     this.id = props.id || uniqueId('field_');
@@ -130,7 +130,7 @@ export class FieldContainer extends React.Component<FieldContainerProps> {
     // Errored form components without fieldsets must handle `aria-invalid` in their own component
     const ariaInvalid = isFieldset && errorMessage ? true : undefined;
 
-    // Bottom placed errors are handled in FieldContainer instead of FormLabel
+    // Bottom placed errors are handled in FormControl instead of FormLabel
     const renderBottomError = bottomError ? (
       <InlineError id={this.errorId} inversed={inversed}>
         {errorMessage}
@@ -143,7 +143,7 @@ export class FieldContainer extends React.Component<FieldContainerProps> {
         <div className="ds-u-visibility--screen-reader">{errorMessage}</div>
       ) : null;
 
-    // Field input props handled by <FieldContainer>
+    // Field input props handled by <FormControl>
     const fieldInputProps = {
       id: this.id,
       labelId: this.labelId,
@@ -176,7 +176,7 @@ export class FieldContainer extends React.Component<FieldContainerProps> {
   }
 }
 
-export const FieldContainerPropKeys = [
+export const FormControlPropKeys = [
   'className',
   'component',
   'errorId',
@@ -195,4 +195,4 @@ export const FieldContainerPropKeys = [
   'render',
 ];
 
-export default FieldContainer;
+export default FormControl;
